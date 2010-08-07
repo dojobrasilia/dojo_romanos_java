f6fb03d8339a0cc984fa883e20246985946a203d
Bruno Pedroso <Bruno@macasa.local>
2010-08-07 12:11:18 -0300

    retro

---
+++ retros.java
@@ -0,0 +1,40 @@
+Retrospectiva dia 07/ago/2010
+
+Presentes:
+	- Bruno Pedroso
+	- Daniel Maffioletti 
+	- Alessandro Leite
+	- Bruno Chaves
+	- Derlon Vandri
+	- Rodrigo Leonardo
+
+O que foi bom?
+	- reativou o dojo-bsb
+	- veio todo mundo que confirmou
+	- todo mundo deu pitaco
+	- solução foi inteligente
+	- terminou o problema
+	- ficou mais claro a ideia do TDD
+	- a retrospectiva foi ótima (bastante tempo)
+
+O Que pode melhorar?
+	- Não comitamos ao longo do problema
+	- Mensagem na exceção da validação
+	- outros notebooks, teclado...
+	- podia tentar um randori
+	- dojo jQuery/QUnit
+	- fazer um dojo usando design por contrato (http://pt.wikipedia.org/wiki/Programa%C3%A7%C3%A3o_por_contrato)
+	- fazer um dojo usando objetos imutáveis
+	- disctuir TDD classistas x behaiviourista (fowler)
+	- outras linguagens
+		
+Discussão:
+	- O horário de sábado é o melhor?
+	- Teria um lugar melhor?
+		- cafés:
+			- blenz
+			- livraria leitura (conjunto)
+			- livraria cultura (iguatemi ou casaPark)
+	- relação entre design by contract x TDD
+	
+	
